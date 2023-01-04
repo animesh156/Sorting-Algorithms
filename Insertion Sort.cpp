999831f9df0d023eff275db45f16b48bc4e829e6
Animesh Kumar Singh <animeshrathore255@gmail.com>
2023-01-04 16:22:03 +0530

    Updated insertion sort

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,37 +1,52 @@
-// Insertion sort in C++
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++) {
-    cout << array[i] << " ";
+void insertionSort(int arr[], int n)
+{
+  for (int i = 1; i < n; i++)
+  {
+    int temp = arr[i];
+    int j = i - 1;
+
+    for (; j >= 0; j--)
+    {
+      if (arr[j] > temp)
+      {
+        arr[j + 1] = arr[j];
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    arr[j + 1] = temp;
+  }
+}
+
+void printArray(int arr[], int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
   }
   cout << endl;
 }
+int main()
+{
+  int arr[] = {6, 2, 9, 4, 1, 8};
+  int n = sizeof(arr) / sizeof(int);
 
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
-    int key = array[step];
-    int j = step - 1;
+  cout << "Unsorted array: ";
 
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key<array[j] to key>array[j].
-    while (key < array[j] && j >= 0) {
-      array[j + 1] = array[j];
-      --j;
-    }
-    array[j + 1] = key;
-  }
-}
+  printArray(arr, n);
 
-// Driver code
-int main() {
-  int data[] = {9, 5, 1, 4, 3};
-  int size = sizeof(data) / sizeof(data[0]);
-  insertionSort(data, size);
-  cout << "Sorted array in ascending order:\n";
-  printArray(data, size);
+  insertionSort(arr, n);
+
+  cout << "Sorted array: ";
+
+  printArray(arr, n);
+
+  return 0;
 }
